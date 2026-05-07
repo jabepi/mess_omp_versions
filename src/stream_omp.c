9c2bb4cb9e133494ea550606342fae40d6d85b3b
Javier <javier@MacBook-Pro-10.local>
2026-05-07 20:13:40 +0200

    debugging

--- src/stream_omp.c
+++ src/stream_omp.c
@@ -455,12 +455,6 @@ int main(int argc, char *argv[])
     if (debug_enabled)
         debug_log_json("Entering ROI parallel section");
     
-    // Trigger Python to switch from ATOMIC CPU to O3 CPU precisely before the ROI
-    printf("Triggering Python to switch from ATOMIC CPU to O3 CPU precisely before the ROI\n");
-    if (m5_enabled)
-        m5_exit(0);
-
-    printf("Python has switched to O3 CPU\n");
 
 #ifdef _OPENMP
         #pragma omp parallel
